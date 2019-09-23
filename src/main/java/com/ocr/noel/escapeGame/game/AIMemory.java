a321ee716e54a7cf368a2906d2e345cc956b3d1c
noel <noeldeprandieres@yahoo.fr>
2019-09-23 15:08:14 +0200

    create class AIMemory and its test class

---
+++ src/main/java/com/ocr/noel/escapeGame/game/AIMemory.java
@@ -0,0 +1,51 @@
+package com.ocr.noel.escapeGame.game;
+
+import com.ocr.noel.escapeGame.utils.ConfigUtil;
+
+/**
+ * This class is the artificial intelligence and its memory
+ * for the computer try to find the secret number
+ */
+public class AIMemory {
+    private int[] numberMin;
+    private int[] numberMax;
+    private int[] numberLastProposal;
+
+    /**
+     * Constructor of artificial intelligence and memory
+     *
+     * @param numberLength length of number to find
+     */
+    public AIMemory(int numberLength) {
+        this.numberMin = new int[numberLength];//fill with 0...
+        this.numberMax = new int[numberLength];
+        for (int i = 0; i < numberLength; i++) {
+            this.numberMax[i] = 10;
+        }
+        this.numberLastProposal = ConfigUtil.generateRandomIntegerArray(numberLength);
+    }
+
+    /**
+     * In this method AI try to find the good number and return it
+     *
+     * @param lastResult the last result of its test for example "+==-"
+     * @return the new array number found
+     */
+    public int[] getNewNumber(String lastResult) {
+        if (lastResult == null || lastResult.length() != numberLastProposal.length) {
+            return this.numberLastProposal;
+        }
+        for (int i = 0; i < lastResult.length(); i++) {
+            switch (lastResult.substring(i, i + 1)) {
+                case "+":
+                    numberMin[i] = numberLastProposal[i];
+                    numberLastProposal[i] = (numberMax[i] + numberLastProposal[i]) / 2;
+                    break;
+                case "-":
+                    numberMax[i] = numberLastProposal[i];
+                    numberLastProposal[i] = (numberMin[i] + numberLastProposal[i]) / 2;
+            }
+        }
+        return numberLastProposal;
+    }
+}
---
+++ src/test/java/com/ocr/noel/escapeGame/game/AIMemoryTest.java
@@ -0,0 +1,30 @@
+package com.ocr.noel.escapeGame.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AIMemoryTest {
+
+    @Test
+    void Given_StringResult_When_getNewNumber_Then_testNewNumber() {
+        AIMemory aiMemory = new AIMemory(4);
+        int[] numberInitialTemp = aiMemory.getNewNumber("");
+        for (int i = 0; i < numberInitialTemp.length; i++) {
+            //force initialization with the values of the array below
+            numberInitialTemp[i] = (new int[]{4, 2, 6, 9})[i];
+        }
+        int[] numberInitial = numberInitialTemp.clone();
+        int[] numberNew = aiMemory.getNewNumber("+==-");
+        assertTrue(numberInitial[0] < numberNew[0]);
+        assertTrue(numberInitial[1] == numberNew[1]);
+        assertTrue(numberInitial[2] == numberNew[2]);
+        assertTrue(numberInitial[3] > numberNew[3]);
+        numberInitial = numberNew.clone();
+        numberNew = aiMemory.getNewNumber("===-");
+        assertTrue(numberInitial[0] == numberNew[0]);
+        assertTrue(numberInitial[1] == numberNew[1]);
+        assertTrue(numberInitial[2] == numberNew[2]);
+        assertTrue(numberInitial[3] > numberNew[3]);
+    }
+}
